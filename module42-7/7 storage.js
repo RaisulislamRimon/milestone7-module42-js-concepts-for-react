0b6da953f086c8463c3d0e0264cf3e0c1e2f697b
RaisulislamRimon <rimonppip0@gmail.com>
2022-09-12 16:58:09 +0600

    localStorage & sessionStorage with json

--- module42-7/7 storage.js
+++ module42-7/7 storage.js
@@ -7,21 +7,43 @@
 // localStorage.clear();
 // console.log(name, age);
 
-document.getElementById("submitBtn").addEventListener("click", function (e) {
-  const task = document.getElementById("task").value;
-  let tasks;
-  if (localStorage.getItem("tasks") === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  }
-  tasks.push(task);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  //   alert("Task saved");
-  e.preventDefault();
-});
+// document.getElementById("submitBtn").addEventListener("click", function (e) {
+//   const task = document.getElementById("task").value;
+//   let tasks;
+//   if (localStorage.getItem("tasks") === null) {
+//     tasks = [];
+//   } else {
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
+//   }
+//   tasks.push(task);
+//   localStorage.setItem("tasks", JSON.stringify(tasks));
+//   //   alert("Task saved");
+//   e.preventDefault();
+// });
 
-document.getElementById("clear").addEventListener("click", function () {
+// document.getElementById("clear").addEventListener("click", function () {
+//   localStorage.clear();
+//   location.reload();
+// });
+
+// function addToLocalStorage() {
+const addToLocalStorage = () => {
+  const storageIdInput = document.getElementById("storage-id");
+  const storageId = storageIdInput.value;
+  const storageValueInput = document.getElementById("storage-value");
+  const storageValue = storageValueInput.value;
+
+  //   console.log(storageIdInput, storageValueInput);
+  //   console.log(storageId, storageValue);
+
+  if (storageId && storageValue) {
+    localStorage.setItem(storageId, storageValue);
+  }
+  storageIdInput.value = "";
+  storageValueInput.value = "";
+};
+
+const clearLocalStorage = () => {
   localStorage.clear();
   location.reload();
-});
+};
